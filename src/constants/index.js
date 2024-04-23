1c675052eec90e5b17b499c6b60b198ee5bf73aa
Monabbir Bhuiyan <85197246+monabbir-hub@users.noreply.github.com>
2024-04-23 09:52:40 -0400

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,358 @@
+import {
+  benefitIcon1,
+  benefitIcon2,
+  benefitIcon3,
+  benefitIcon4,
+  benefitImage2,
+  chromecast,
+  disc02,
+  discord,
+  discordBlack,
+  facebook,
+  figma,
+  file02,
+  framer,
+  homeSmile,
+  instagram,
+  notification2,
+  notification3,
+  notification4,
+  notion,
+  photoshop,
+  plusSquare,
+  protopie,
+  raindrop,
+  recording01,
+  recording03,
+  roadmap1,
+  roadmap2,
+  roadmap3,
+  roadmap4,
+  searchMd,
+  slack,
+  sliders04,
+  telegram,
+  twitter,
+  yourlogo,
+} from "../assets";
+
+export const navigation = [
+  {
+    id: "0",
+    title: "Features",
+    url: "#features",
+  },
+  {
+    id: "1",
+    title: "Pricing",
+    url: "#pricing",
+  },
+  {
+    id: "2",
+    title: "How to use",
+    url: "#how-to-use",
+  },
+  {
+    id: "3",
+    title: "About Us",
+    url: "#aboutus",
+  },
+  {
+    id: "4",
+    title: "New account",
+    url: "#signup",
+    onlyMobile: true,
+  },
+  {
+    id: "5",
+    title: "Sign in",
+    url: "#login",
+    onlyMobile: true,
+  },
+];
+
+export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
+
+export const notificationImages = [notification4, notification3, notification2];
+
+export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
+
+export const brainwaveServices = ["Monabbir", "Samiul", "Bhai Bhai"];
+
+export const brainwaveServicesIcons = [
+  recording03,
+  recording01,
+  disc02,
+  chromecast,
+  sliders04,
+];
+
+export const roadmap = [
+  {
+    id: "0",
+    title: "Voice recognition",
+    text: "Enable the chatbot to understand and respond to voice commands, making it easier for users to interact with the app hands-free.",
+    date: "May 2023",
+    status: "done",
+    imageUrl: roadmap1,
+    colorful: true,
+  },
+  {
+    id: "1",
+    title: "Gamification",
+    text: "Add game-like elements, such as badges or leaderboards, to incentivize users to engage with the chatbot more frequently.",
+    date: "May 2023",
+    status: "progress",
+    imageUrl: roadmap2,
+  },
+  {
+    id: "2",
+    title: "Chatbot customization",
+    text: "Allow users to customize the chatbot's appearance and behavior, making it more engaging and fun to interact with.",
+    date: "May 2023",
+    status: "done",
+    imageUrl: roadmap3,
+  },
+  {
+    id: "3",
+    title: "Integration with APIs",
+    text: "Allow the chatbot to access external data sources, such as weather APIs or news APIs, to provide more relevant recommendations.",
+    date: "May 2023",
+    status: "progress",
+    imageUrl: roadmap4,
+  },
+];
+
+export const collabText =
+  "Centralize, Automate, Accelerate – ConvoCloud at the Heart of Your Workflow.";
+
+export const collabContent = [
+  {
+    id: "0",
+    title: "Seamless Integration",
+    text: "Effortlessly sync ConvoCloud with your existing tools and workflows. Enjoy a seamless experience that bridges the gap between customer support and your team's productivity tools.",
+  },
+  {
+    id: "1",
+    title: "Intelligent Automation",
+    text: "Our AI-driven workflows provide timely customer responses, automate routine tasks, and route complex issues to the right team, boosting efficiency across the board.",
+  },
+  {
+    id: "2",
+    title: "Robust Security",
+    text: "With industry-leading encryption and compliance with global data protection standards, ConvoCloud keeps your conversations secure and your data protected.",
+  },
+  {
+    id: "3",
+    title: "24/7 Support",
+    text: "Empower your business with round-the-clock customer support. ConvoCloud's AI doesn't sleep, ensuring your customers always have assistance, anytime, anywhere.",
+  },
+  {
+    id: "4",
+    title: "Customizable AI Personalities",
+    text: "Craft the voice of your brand with customizable chatbot personalities, creating a unique and engaging customer experience.",
+  },
+  {
+    id: "5",
+    title: "In-Depth Analytics",
+    text: "Leverage powerful insights from customer interactions to make data-driven decisions that enhance service quality and drive retention.",
+  },
+  {
+    id: "6",
+    title: "Scalable Solutions",
+    text: "Whether you're a startup or a Fortune 500, ConvoCloud grows with you. Easily adjust to handle any volume of customer queries without skipping a beat.",
+  },
+];
+
+export const collabApps = [
+  {
+    id: "0",
+    title: "Figma",
+    icon: figma,
+    width: 26,
+    height: 36,
+  },
+  {
+    id: "1",
+    title: "Notion",
+    icon: notion,
+    width: 34,
+    height: 36,
+  },
+  {
+    id: "2",
+    title: "Discord",
+    icon: discord,
+    width: 36,
+    height: 28,
+  },
+  {
+    id: "3",
+    title: "Slack",
+    icon: slack,
+    width: 34,
+    height: 35,
+  },
+  {
+    id: "4",
+    title: "Photoshop",
+    icon: photoshop,
+    width: 34,
+    height: 34,
+  },
+  {
+    id: "5",
+    title: "Protopie",
+    icon: protopie,
+    width: 34,
+    height: 34,
+  },
+  {
+    id: "6",
+    title: "Framer",
+    icon: framer,
+    width: 26,
+    height: 34,
+  },
+  {
+    id: "7",
+    title: "Raindrop",
+    icon: raindrop,
+    width: 38,
+    height: 32,
+  },
+];
+
+export const pricing = [
+  {
+    id: "0",
+    title: "Starter Plan",
+    description: "AI-Powered Support Essentials",
+    price: "35",
+    time: "/mo",
+    features: [
+      "Standard customer support automation",
+      "Access to basic analytics and reporting",
+      "Email support with a 48-hour response time",
+    ],
+  },
+  {
+    id: "1",
+    title: "Professional Plan",
+    description: "Advanced Features for Growing Teams",
+    price: "75",
+    time: "/mo",
+    features: [
+      "Enhanced AI chatbot with deep learning comprehension",
+      "Advanced personalization for customer interactions",
+      "Full analytics suite with trend insights",
+      "Integration with CRM and third-party services",
+      "Priority email and chat support",
+    ],
+  },
+  {
+    id: "2",
+    title: "Enterprise Plan",
+    description: "Full-Scale AI for Enterprise Excellence",
+    price: "150",
+    time: "/mo",
+    features: [
+      "State-of-the-art AI chatbot with continuous learning",
+      "Comprehensive personalization with predictive analytics",
+      "Custom integration capabilities tailored to your business",
+      "Real-time dashboards and in-depth performance tracking",
+      "24/7 premium support with a dedicated account manager",
+      "Regular consultations for optimization and strategic planning",
+    ],
+  },
+];
+
+export const benefits = [
+  {
+    id: "0",
+    title: "Seamless Integration",
+    text: "ConvoCloud effortlessly blends into your existing website framework, enabling immediate AI-driven interaction. No extensive setup required – just plug and play for instant support and engagement.",
+    backgroundUrl: "assets/benefits/card-1.svg",
+    iconUrl: benefitIcon1,
+    imageUrl: benefitImage2,
+    light: true,
+  },
+  {
+    id: "1",
+    title: "Real-Time Resolutions",
+    text: "Experience the power of real-time problem solving. ConvoCloud provides instant answers to customer inquiries, boosting satisfaction and reducing wait times with smart, AI-powered conversations.",
+    backgroundUrl: "assets/benefits/card-2.svg",
+    iconUrl: benefitIcon2,
+    imageUrl: benefitImage2,
+    light: true,
+  },
+  {
+    id: "2",
+    title: "Personalized Interactions",
+    text: "Every customer conversation is an opportunity for personalized service. ConvoCloud’s AI learns from each interaction, ensuring that every response is tailored to individual customer preferences and history.",
+    backgroundUrl: "assets/benefits/card-3.svg",
+    iconUrl: benefitIcon3,
+    imageUrl: benefitImage2,
+    light: true,
+  },
+  {
+    id: "3",
+    title: "Data-Driven Decisions",
+    text: "Unlock the insights hidden in your customer interactions. ConvoCloud turns conversations into valuable data, helping you make informed decisions that drive business growth and customer loyalty.",
+    backgroundUrl: "assets/benefits/card-4.svg",
+    iconUrl: benefitIcon4,
+    imageUrl: benefitImage2,
+    light: true,
+  },
+  {
+    id: "4",
+    title: "Always-On Service",
+    text: "Round-the-clock support doesn’t have to be costly. With ConvoCloud, provide 24/7 assistance to your customers, ensuring they always have help when they need it, without stretching your resources thin.",
+    backgroundUrl: "assets/benefits/card-5.svg",
+    iconUrl: benefitIcon1,
+    imageUrl: benefitImage2,
+    light: true,
+  },
+  {
+    id: "5",
+    title: "Scalable Conversations",
+    text: "Whether you're serving ten customers or ten thousand, ConvoCloud scales to meet demand. Our AI-driven chatbots handle the increased volume without sacrificing the quality of customer interactions.",
+    backgroundUrl: "assets/benefits/card-6.svg",
+    iconUrl: benefitIcon2,
+    imageUrl: benefitImage2,
+    light: true,
+  },
+];
+
+export const socials = [
+  {
+    id: "0",
+    title: "Discord",
+    iconUrl: discordBlack,
+    url: "#",
+  },
+  {
+    id: "1",
+    title: "Twitter",
+    iconUrl: twitter,
+    url: "#",
+  },
+  {
+    id: "2",
+    title: "Instagram",
+    iconUrl: instagram,
+    url: "#",
+  },
+  {
+    id: "3",
+    title: "Telegram",
+    iconUrl: telegram,
+    url: "#",
+  },
+  {
+    id: "4",
+    title: "Facebook",
+    iconUrl: facebook,
+    url: "#",
+  },
+];
